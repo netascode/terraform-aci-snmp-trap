34808117c3b3503c7034cb10714ae8d9c1f763ce
danischm <danischm@cisco.com>
2021-10-11 20:30:28 +0200

    Update implicit resource dependencies

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aci_rest" "snmpGroup" {
 
 resource "aci_rest" "snmpTrapDest" {
   for_each   = { for dest in var.destinations : dest.hostname_ip => dest }
-  dn         = "${aci_rest.snmpGroup.id}/trapdest-${each.value.hostname_ip}-port-${each.value.port != null ? each.value.port : 162}"
+  dn         = "${aci_rest.snmpGroup.dn}/trapdest-${each.value.hostname_ip}-port-${each.value.port != null ? each.value.port : 162}"
   class_name = "snmpTrapDest"
   content = {
     host     = each.value.hostname_ip
@@ -22,7 +22,7 @@ resource "aci_rest" "snmpTrapDest" {
 
 resource "aci_rest" "fileRsARemoteHostToEpg" {
   for_each   = { for dest in var.destinations : dest.hostname_ip => dest if dest.mgmt_epg_name != null }
-  dn         = "${aci_rest.snmpTrapDest[each.value.hostname_ip].id}/rsARemoteHostToEpg"
+  dn         = "${aci_rest.snmpTrapDest[each.value.hostname_ip].dn}/rsARemoteHostToEpg"
   class_name = "fileRsARemoteHostToEpg"
   content = {
     tDn = each.value.mgmt_epg_type == "oob" ? "uni/tn-mgmt/mgmtp-default/oob-${each.value.mgmt_epg_name}" : "uni/tn-mgmt/mgmtp-default/inb-${each.value.mgmt_epg_name}"
